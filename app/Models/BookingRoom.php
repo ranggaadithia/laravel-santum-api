1c039f546ad2f2cb0151a6c67ce4dcc4c8711677
Rangga Adithia <rangga.adithia26@gmail.com>
2024-06-25 00:16:28 +0800

    Add booking room functionality to User and Room models
    The code changes in the User and Room models include adding a new relationship method called . This method establishes a one-to-many relationship between the User and BookingRoom models, allowing a user to have multiple booking rooms. This functionality is necessary for managing room bookings in the application.
    
    The commit message follows the established convention of starting with a verb in the imperative form (\Add\) to describe the purpose of the changes.

---
+++ app/Models/BookingRoom.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BookingRoom extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
+
+    static function isBookingConflict($roomId, $bookingDate, $startTime, $endTime)
+    {
+        return BookingRoom::where('room_id', $roomId)
+            ->where('booking_date', $bookingDate)
+            ->where(function ($query) use ($startTime, $endTime) {
+                $query->whereBetween('start_time', [$startTime, $endTime])
+                    ->orWhereBetween('end_time', [$startTime, $endTime]);
+            })
+            ->exists();
+    }
+}
---
+++ database/migrations/2024_06_24_155039_create_booking_rooms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('booking_rooms', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('room_id')->constrained()->cascadeOnDelete();
+            $table->date('booking_date');
+            $table->text('reason_for_booking');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('booking_rooms');
+    }
+};
